e817d75f9e9595d032075db74b2a173836110e81
Skablam <stainlesssteelmatt@gmail.com>
2015-02-13 21:44:47 +0000

    amended message again

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World, WOO, YEAH!'
+    return 'Hello World, WOOO, YEAH!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
